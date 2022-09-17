3b507b643fe571259112d17e1b9b3f80fe20e91d
Sirish Patibanda <Sirishpatibanda1@gmail.com>
2022-09-17 17:46:21 -0500

    File changes

--- CalcFunctions.py
+++ CalcFunctions.py
@@ -4,8 +4,12 @@ import sympy as simp
 x = simp.symbols("x")
 eq = simp.parse_expr(input())
 def integrals(eq):
+    x = simp.symbols("x")
+    eq = simp.parse_expr(input())
+
     return simp.integrate(eq,x)
+
 def derivative(eq):
+    x = simp.symbols("x")
+    eq = simp.parse_expr(input())
     return  simp.diff(eq, x)
-print(integrals(eq))
-print(derivative(eq))
