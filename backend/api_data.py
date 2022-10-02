0fc80856456e50aab0b167d8628ed0f2a5aa2413
vadit <59973483+vadit@users.noreply.github.com>
2022-10-02 01:45:40 -0500

    Update api_data.py

--- backend/api_data.py
+++ backend/api_data.py
@@ -1,46 +1,75 @@
-from flask import Flask
-from flask_cors import CORS
-from flask import request
 import requests
 import json
-arrival = ''
-destination = ''
-num_adults = ''
-departure_date = ''
-arrival_date = ''
-api = Flask(__name__)
-CORS(api)
-@api.route('/profile')
-def my_profile():
-    response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
-    }
-    return response_body
+from hotels_api_data import *
 
-@api.route('/data/<arrival>&<destination>')
-def run_api(arrival,destination):   
-    print(arrival)
-    print(destination)
-    x = arrival[8:len(arrival)]
-    y = destination[12:len(destination)]
+
+#local location data
+localLocationCity = ''
+localLocationState = ''
+localLocationCountry = ''
+
+#destination data
+destinationCity = ''
+destinationState = ''
+destinationCountry = ''
+
+num_adults = ''
+departureDate = ''
+arrival_date = ''
+
+
+def run_api(arrival, destination, departure_date,num_adults):
     url = "https://priceline-com-provider.p.rapidapi.com/v2/flight/departures"
-    querystring = {"sid": "iSiX639", "departure_date": "2022-10-05", "adults": 1,
-                   "origin_airport_code": x, "destination_airport_code": y}
+
+    querystring = {"sid": "iSiX639", "departure_date": departure_date, "adults": num_adults,
+                   "origin_airport_code": arrival, "destination_airport_code": destination}
 
     headers = {
         "X-RapidAPI-Key": "75b97bcf14msh05aaf11d45af270p144a1djsn97dba5f65bc6",
         "X-RapidAPI-Host": "priceline-com-provider.p.rapidapi.com"
-        
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
-  
     y = json.loads(response.text)
-    airlineData = y['getAirFlightDepartures']['results']['result']['airline_data']
-    returnA = []
+
+    cityData = y['getAirFlightDepartures']['results']['result']['search_data']['search_0'] # getting city data
+
+    
+    global localLocationCity 
+    localLocationCity = cityData['origin']['city']
+
+    global localLocationState
+    localLocationState = cityData['origin']['state']
+
+    global localLocationCountry
+    localLocationCountry = cityData['origin']['country']
+    
+    global destinationCity 
+    destinationCity = cityData['destination']['city']
+
+    global destinationState
+    destinationState = cityData['destination']['state']
+
+    global destinationCountry
+    destinationCountry = cityData['destination']['country']
+
+    global departureDate
+    departureDate = departure_date
+    
+
+    airlineData = y['getAirFlightDepartures']['results']['result']['airline_data'] # getting airline data
+    allInfo = []
 
     for x in airlineData.keys():
-        returnA.append(airlineData[x]['name'])
-    print(returnA)
-    return returnA
+        temp = []
+        temp.append(airlineData[x]['name'])
+        temp.append(airlineData[x]['code'])
+        temp.append(airlineData[x]['websiteUrl'])
+        temp.append(airlineData[x]['phoneNumber'])
+        allInfo.append(temp)
+
+
+    hotelsAndPlanes = []
+    hotelsAndPlanes.append(allInfo)
+    hotelsAndPlanes.append(getCities(destinationCity, destinationState, destinationCountry, departure_date))
+    return hotelsAndPlanes
